2c90ebbea4e1538d0317ea83a834b421122ac463
ClaudiaLie <claudiss.lai@gmail.com>
2021-06-07 18:34:40 +0000

    Add Session User Profile

--- app.py
+++ app.py
@@ -45,6 +45,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -62,6 +64,7 @@ def login():
                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -71,9 +74,16 @@ def login():
             # username doesn't exists
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
-  
+
     return render_template("login.html")
 
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's name from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 @app.route("/recipes")
@@ -84,4 +94,4 @@ def recipes():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")),
-     debug=True)
+    debug=True)
